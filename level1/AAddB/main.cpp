5639ed0f6ccea65a30ac628f8069f79a09ac4fda
fengxuan <651353997@qq.com>
2016-07-22 14:54:59 +0800

    test first level1

---
+++ level1/AAddB/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+//
+// Created by fengxuan on 2016/7/22.
+//
+
+using namespace std;
+
+
+int main() {
+    int a,b;
+    while(cin >> a >> b){
+        cout << a + b << endl;
+    }
+    return 0;
+}
+
---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+bool IsPopOrder(vector<int> pushV,vector<int> popV) {
+
+}
+
+int main() {
+    vector<int> s1 = {1,2,3,4,5};
+    vector<int> s2 = {4,5,3,2,1};
+
+    cout << IsPopOrder(s1,s2) << endl;
+
+    cout << "Hello, World!" << endl;
+    return 0;
+}
